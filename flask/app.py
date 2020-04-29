496d4d171640b2e8930cb23c0d5d748d890fadc9
Sam Thurman <sam.bbmgmt@gmail.com>
2020-04-29 16:23:35 -0700

    flask app overhaul: app up and running, model implemented, images feeding to model for predictions on results.html template

--- flask/app.py
+++ flask/app.py
@@ -1,9 +1,21 @@
-from flask import Flask, send_from_directory, render_template, request, abort
 from waitress import serve
-from src.models.wine_predictor import predict_wine
-from src.utils import validate_input
-
+from src.utils import *
+from flask import Flask, send_from_directory, render_template, request, abort
 app = Flask(__name__, static_url_path="/static")
+# from src.models.wine_predictor import predict_wine
+
+'''
+to run flask app in development mode from terminal
+export FLASK_ENV=development
+env FLASK_APP=app.py flask run
+'''
+
+key_dict = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4,
+            'F': 5, 'G': 6, 'H': 7, 'I': 8, 'J': 9,
+            'K': 10, 'L': 11, 'M': 12, 'N': 13, 'O': 14,
+            'P': 15, 'Q': 16, 'R': 17, 'S': 18, 'T': 19,
+            'U': 20, 'V': 21, 'W': 22, 'X': 23, 'Y': 24,
+            'Z': 25}
 
 
 @app.route("/")
@@ -16,15 +28,15 @@ def index():
 def get_results():
     """ Predict the class of wine based on the inputs. """
     data = request.form
-    print(data)
-
-    test_value, errors = validate_input(data)
-
-    if not errors:
-        predicted_class = predict_wine(test_value)
-        return render_template("results.html", predicted_class=predicted_class)
-    else:
-        return abort(400, errors)
+    # print(data)
+    model = load_saved_model('copy_model3.h5')
+    filename = data['image_name']
+    true_label = data['image_true_label']
+    image = preprocess_for_predict(filename)
+    prediction_index = predict_on(model, image)
+    letter_predict = list(key_dict.keys())[list(
+        key_dict.values()).index(prediction_index)]
+    return render_template("results.html", predicted_class=letter_predict, file_name=filename, true_label=true_label)
 
 
 if __name__ == "__main__":
--- flask/src/utils.py
+++ flask/src/utils.py
@@ -1,3 +1,40 @@
 '''
 Code for making predictions with model(s) stored in flask/src/models
 '''
+import numpy as np
+
+import skimage
+from skimage.color import rgb2gray
+from skimage import io
+from skimage import transform
+
+import keras
+import tensorflow
+from keras.models import load_model
+
+
+def load_saved_model(model_name):
+    '''
+    This function takes in a saved model name as a string and returns the loaded model
+    (copy_model3.h5)
+    '''
+    print('running')
+    model_path = f'../models/{model_name}'
+    return keras.models.load_model(model_path)
+
+
+def preprocess_for_predict(file_name):
+    data_path = f'src/test_images/{file_name}'
+    Xi = io.imread(data_path)
+    Xi = rgb2gray(Xi)
+    Xi = transform.resize(Xi, (128, 128))
+    Xi = (Xi - 0.5)*2
+    Xi = np.expand_dims(Xi, axis=2)
+    Xi = np.expand_dims(Xi, axis=0)
+    return Xi
+
+
+def predict_on(model, image):
+    soft = model.predict(image)
+    pred = np.argmax(soft)
+    return pred
